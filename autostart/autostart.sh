c5355a6d0354e5eb3f18d0c0c10e03800a9e28bd
cy <root@rescld.cn>
2020-03-22 18:08:53 +0800

    Create

---
+++ autostart/autostart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# AutoStart
+compton &
+# variety &
+fcitx &
+feh --bg-scale /home/cy/Images/54.jpeg &
+# oneko &
+# screenkey &
+
+# TIM gnome-settings-daemon
+/usr/lib/gsd-xsettings &
